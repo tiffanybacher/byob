1fcaaee34669565d0d0411c58ae214ca31e51855
tiffanybacher <tiffanybacher@gmail.com>
2019-06-25 21:39:43 -0600

    Create houses data

---
+++ datasets/housesData.js
@@ -0,0 +1,26 @@
+export default const housesData = [
+  {
+    house: 'Gryffindor',
+    founder: 'Godric Gryffindor',
+    animal: 'lion',
+    colors: 'red and gold'
+  },
+  {
+    house: 'Ravenclaw',
+    founder: 'Rowena Ravenclaw',
+    animal: 'eagle',
+    colors: 'blue and bronze'
+  },
+  {
+    house: 'Hufflepuff',
+    founder: 'Helga Hufflepuff',
+    animal: 'badger',
+    colors: 'yellow and black',
+  },
+  {
+    house: 'Slytherin',
+    founder: 'Salazar Slytherin',
+    animal: 'snake',
+    colors: 'green and silver'
+  }
+];
