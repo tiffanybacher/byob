bf467a1f3ff87db648d07c34e265549afd32d31c
tiffanybacher <tiffanybacher@gmail.com>
2019-06-25 21:38:08 -0600

    Migrate initial.js

--- db/migrations/20190625213316_initial.js
+++ db/migrations/20190625213316_initial.js
@@ -8,42 +8,42 @@ exports.up = function(knex, Promise) {
       table.string('animal');
       table.string('colors');
       table.timestamps(true, true);
-    });
+    }),
 
     knex.schema.createTable('staff', table => {
       table.increments('id').primary();
       table.string('name');
       table.string('job_title');
-    });
+    }),
 
     knex.schema.createTable('students', table => {
       table.increments('id').primary();
       table.string('name');
       table.integer('house_id').unsigned().references('houses.id');
       table.timestamps(true, true);
-    });
+    }),
 
     knex.schema.createTable('classes', table => {
       table.increments('id').primary();
       table.string('class');
       table.integer('instructor_id').unsigned().references('staff.id');
       table.timestamps(true, true);
-    });
+    }),
 
     knex.schema.createTable('classrooms', table => {
       table.integer('class_id').unsigned().references('classes.id');
       table.integer('student_id').unsigned().references('students.id');
       table.timestamps(true, true);
-    });
+    })
   ]);
 };
 
 exports.down = function(knex, Promise) {
   return Promise.all([
-    knex.schema.dropTable('houses');
-    knex.schema.dropTable('staff');
-    knex.schema.dropTable('students');
-    knex.schema.dropTable('classes');
-    knex.schema.dropTable('classrooms');
+    knex.schema.dropTable('houses'),
+    knex.schema.dropTable('staff'),
+    knex.schema.dropTable('students'),
+    knex.schema.dropTable('classes'),
+    knex.schema.dropTable('classrooms')
   ]);
 };
