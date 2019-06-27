5923567139665aa37d7c17ffc35ccdaa945930d9
tiffanybacher <tiffanybacher@gmail.com>
2019-06-27 11:53:03 -0600

    Write get and post endpoints

--- db/migrations/20190626133214_edit-house-id.js
+++ db/migrations/20190626133214_edit-house-id.js
@@ -1,16 +1,16 @@
 
-// exports.up = function(knex) {
-//   return Promise.all([
-//     knex.schema.alterTable('houses', table => {
-//       table.integer('id').primary().alter();
-//     })
-//   ]);
-// };
+exports.up = function(knex) {
+  return Promise.all([
+    knex.schema.createTable('', table => {
 
-// exports.down = function(knex) {
-//   return Promises.all([
-//     knex.schema.alterTable('houses', table => {
-//       table.increment('id').primary().alter();
-//     })
-//   ]);
-// };
+    })
+  ]);
+};
+
+exports.down = function(knex) {
+  return Promises.all([
+    knex.schema.createTable('', table => {
+
+    })
+  ]);
+};
--- server.js
+++ server.js
@@ -11,8 +11,80 @@ app.listen(3001, () => {
   console.log(`Hogwarts is running on port ${port}`);
 });
 
+app.post('/api/v1/staff', (request, response) => {
+  const staffMember = request.body;
+
+  for (let param of ['name', 'job_title']) {
+    if (!staffMember[param]) {
+      return response
+        .status(422)
+        .send({ error: `Expected { name: <string>, job_title: <string> }.` });
+    }
+  }
+
+  database('staff').insert(staffMember, 'id')
+    .then(staffMember => {
+      response.status(201).json({ id: staffMember[0] });
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
 app.get('/api/v1/staff', (request, response) => {
   database('staff').select()
     .then(staff => response.status(200).json(staff))
     .catch(error => response.status(500).json({ error }));
 });
+
+app.get('/api/v1/staff/:id', (request, response) => {
+  database('staff').where('id', request.params.id).select()
+    .then(staffMember => {
+      if (staffMember.length) {
+        response.status(200).json(staffMember);
+      } else {
+        response.status(404).json({
+          error: `Could not find staff member with id ${request.params.id}.`
+        });
+      }
+    })
+    .catch(error => response.status(500).json({ error }));
+});
+
+app.post('/api/v1/students', (request, response) => {
+  const student = request.body;
+
+  for (let param of ['name', 'house_id']) {
+    if (!student[param]) {
+      return response.status(422)
+        .send({ error: `Expected { name: <string>, house_id: <integer> }. "${param}" is missing.` });
+    }
+  }
+
+  database('students').insert(student, 'id')
+    .then(footnote => {
+      response.status(201).json({ id: footnote[0] });
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    })
+});
+
+app.get('/api/v1/students', (request, response) => {
+  database('students').select()
+    .then(students => response.status(200).json(students))
+    .catch(error => response.status(500).json({ error }));
+});
+
+app.get('/api/v1/students/:id', (request, response) => {
+  database('students').where('id', request.params.id).select()
+    .then(student => {
+      if (student.length) {
+        response.status(200).json(student);
+      } else {
+        response.status(404).json({ error });
+      }
+    })
+    .catch(error => `Could not find student with id ${request.params.id}.` );
+});
+
