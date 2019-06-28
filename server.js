e2ec4a666511d2b5c85b92ff838c1c158af6afce
tiffanybacher <tiffanybacher@gmail.com>
2019-06-28 09:57:24 -0600

    Update port in server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = app.set('port', process.env.PORT || 3001);
 
 app.use(express.json());
 
