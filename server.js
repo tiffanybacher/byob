284d09bdea98c32bfe37530b549be05a504dd0df
tiffanybacher <tiffanybacher@gmail.com>
2019-06-28 10:00:40 -0600

    Update app.listen

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 const express = require('express');
 const app = express();
-const port = app.set('port', process.env.PORT || 3001);
 
 app.use(express.json());
 
-app.listen(3001, () => {
+app.listen(process.env.PORT || 3001, () => {
   console.log(`Hogwarts is running on port ${port}`);
 });
 
