4d3ad617f3252ec9f020c3560043d1d2fd1034c8
tiffanybacher <tiffanybacher@gmail.com>
2019-06-28 09:49:14 -0600

    Add production to knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,5 @@
 
 module.exports = {
-
   development: {
     client: 'pg',
     connection: 'postgres://localhost/hogwarts',
@@ -12,5 +11,12 @@ module.exports = {
       directory: './db/seeds/dev'
     }
   },
-
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
+  }
 };
